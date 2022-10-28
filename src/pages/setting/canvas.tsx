f30a654611e06f764672fb23ff8c74874a099ace
lisiur <lisiurday@gmail.com>
2022-10-28 23:29:16 +0800

    wip: setting

--- src/pages/setting/canvas.tsx
+++ src/pages/setting/canvas.tsx
@@ -1,8 +1,10 @@
-import { computed, defineComponent, onMounted, PropType, reactive, ref } from "vue";
+import { computed, defineComponent, nextTick, onMounted, PropType, reactive, ref } from "vue";
+import { NDropdown } from 'naive-ui'
 import { ConfigSchema, LinkSchema } from "./types";
 import { vFor, vIf } from "../../utils/jsxHelper";
 import { Setting, nodeId } from "./lib/setting";
 import "./lib/setting.css"
+import { useContextmenu } from "./contextmenu";
 
 const props = {
     config: {
@@ -14,31 +16,51 @@ export default defineComponent({
     props,
     setup(props) {
         const rootDom = ref<HTMLElement | null>(null)
-        const setting = ref<Setting | null>(null)
 
         const nodes = reactive(props.config?.nodes ?? [])
 
+        let setting!: Setting;
         onMounted(() => {
-            setting.value = new Setting(rootDom.value!, props.config!)
+            setting = new Setting(rootDom.value!, props.config!)
         })
+
+        const { Contextmenu, show: showContextmenu } = useContextmenu()
+
+        function nodeContextmenuHandler(e: MouseEvent) {
+            e.preventDefault();
+            showContextmenu(e, [{
+                key: "node:config",
+                label: "Config",
+            }, {
+                key: "node:delete",
+                label: "Delete",
+            }], (key) => {
+                alert(key)
+            })
+        }
 
         return {
             rootDom,
             nodes,
+            showContextmenu,
+            Contextmenu,
+            nodeContextmenuHandler,
         }
     },
     render() {
         return <div ref="rootDom" class="relative">
             {
-                vFor(this.nodes, (node) => <div 
-                    class="setting-node w-28 h-12 border absolute" 
+                vFor(this.nodes, (node) => <div
+                    class="setting-node w-28 h-12 border absolute"
                     id={nodeId(node.id)}
                     style={{
                         left: node.x + 'px',
                         top: node.y + 'px',
                     }}
-                    >{node.type}</div>)
+                    onContextmenu={this.nodeContextmenuHandler}
+                >{node.type}</div>)
             }
+            <this.Contextmenu></this.Contextmenu>
         </div>
     }
 })
---
+++ src/pages/setting/contextmenu.tsx
@@ -0,0 +1,63 @@
+import { defineComponent, nextTick, reactive, ref } from "vue";
+import { NDropdown, DropdownProps } from 'naive-ui'
+
+type ArrayItem<T extends Array<any>> = T extends Array<infer U> ? U : never
+type DropdownOption = ArrayItem<Exclude<DropdownProps['options'], undefined>>
+
+function getMouseOffset(e: MouseEvent) {
+    const target = e.target as HTMLElement
+    return {
+        x: target.offsetLeft + e.offsetX,
+        y: target.offsetTop + e.offsetY,
+    }
+}
+
+export function useContextmenu() {
+    const showContextmenu = ref(false)
+    const contextmenuPos = reactive({
+        x: 0,
+        y: 0,
+    })
+    const contextmenuOptions = ref<Array<any>>([])
+    let selectMenuHandler = ref((key: string) => { hide() })
+
+    function show(e: MouseEvent, options: Array<DropdownOption>, handler: (key: string) => void) {
+        const pos = getMouseOffset(e)
+        contextmenuPos.x = pos.x
+        contextmenuPos.y = pos.y
+        selectMenuHandler.value = (key: string) => {
+            handler(key)
+            hide()
+        }
+        showContextmenu.value = false
+        nextTick(() => {
+            contextmenuOptions.value = options
+            showContextmenu.value = true
+        })
+    }
+
+    function hide() {
+        showContextmenu.value = false
+    }
+
+    const Contextmenu = defineComponent({
+        render() {
+            return <NDropdown
+                placement="bottom-start"
+                trigger="manual"
+                x={contextmenuPos.x}
+                y={contextmenuPos.y}
+                show={showContextmenu.value}
+                options={contextmenuOptions.value}
+                onClickoutside={hide}
+                onSelect={selectMenuHandler.value}
+            ></NDropdown>
+        }
+    })
+
+    return {
+        Contextmenu,
+        show,
+        hide,
+    }
+}
